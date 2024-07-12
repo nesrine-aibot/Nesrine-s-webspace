f6064635c40daed11903a0512e90c51737116a58
nesrine-aibot <124919116+nesrine-aibot@users.noreply.github.com>
2024-07-12 01:05:06 +0000

    modify the structure and add some routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , send_from_directory
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return render_template('index.html')
+  
+@app.route('/form.html')
+def form(): 
+  return render_template('form.html')
+
+# Route to serve publication.json
+@app.route('/publication.json')
+def get_publication():
+    return send_from_directory('.', 'publication.json')
 
 
 if __name__ == "__main__":
