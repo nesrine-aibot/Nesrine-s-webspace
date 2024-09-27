0475f8cf4cc5af0369affb651dc918da12309f97
nesrine-aibot <124919116+nesrine-aibot@users.noreply.github.com>
2024-09-27 16:08:55 +0200

    Update app.py
    i added just a comment to test a thing in the commit process

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template , send_from_directory
 
 app = Flask(__name__)
 
-
+# this is just a dump comment to ssay hello it's meeee
 @app.route('/')
 def index():
   return render_template('index.html')
